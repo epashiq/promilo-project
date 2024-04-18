eaf1e95b1f294a06ea2f1b100238ab5bfe670e6f
epashiq <ashiqashi4715@gmail.com>
2024-04-18 22:10:37 +0530

    service class created and added riverpod packages

--- lib/controller/services/auth_service.dart
+++ lib/controller/services/auth_service.dart
@@ -1,43 +1,44 @@
-// import 'dart:convert';
-// import 'dart:developer';
+import 'dart:convert';
+import 'dart:developer';
 
-// import 'package:crypto/crypto.dart';
-// import 'package:dio/dio.dart';
-// import 'package:promilo_meetup/core/utils/api_utils.dart';
-// import 'package:promilo_meetup/model/token_model.dart';
-// import 'package:promilo_meetup/model/user_model.dart';
+import 'package:crypto/crypto.dart';
+import 'package:dio/dio.dart';
+import 'package:promilo_project/model/auth_model.dart';
 
-// class AuthService {
-//   static Future<TokenModel?> login(UserModel model) async {
-//     final dio = Dio();
-//     final convertedPassword =
-//         sha256.convert(utf8.encode(model.password.trim())).toString();
+import 'package:promilo_project/model/token_model.dart';
+import 'package:promilo_project/utils/api_utils.dart';
 
-//     final formData = FormData.fromMap({
-//       'username': model.email,
-//       'password': convertedPassword,
-//       'grant_type': 'password',
-//     });
+class AuthService {
+  static Future<TokenModel?> login(AuthModel model) async {
+    final dio = Dio();
+    final convertedPassword =
+        sha256.convert(utf8.encode(model.password.trim())).toString();
 
-//     try {
-//       final response = await dio.post(
-//         ApiUtils.postUrl,
-//         data: formData,
-//         options: Options(
-//           headers: {
-//             'Authorization': 'Basic UHJvbWlsbzpxNCE1NkBaeSN4MiRHQg==',
-//           },
-//         ),
-//       );
+    final formData = FormData.fromMap({
+      'username': model.email,
+      'password': convertedPassword,
+      'grant_type': 'password',
+    });
 
-//       if (response.statusCode == 200) {
-//         return TokenModel(token: response.data['response']['access_token']);
-//       } else {
-//         log('Status code: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       log(e.toString());
-//     }
-//     return null;
-//   }
-// }
+    try {
+      final response = await dio.post(
+        ApiUtils.authUrl,
+        data: formData,
+        options: Options(
+          headers: {
+            'Authorization': 'Basic UHJvbWlsbzpxNCE1NkBaeSN4MiRHQg==',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        return TokenModel(token: response.data['response']['access_token']);
+      } else {
+        log('Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
---
+++ lib/utils/api_utils.dart
@@ -0,0 +1,3 @@
+class ApiUtils {
+  static const authUrl = 'https://apiv2stg.promilo.com/user/oauth/token';
+}
