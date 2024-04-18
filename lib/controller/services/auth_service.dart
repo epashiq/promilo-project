b380212728eac95ab5f77aaf08df213cc23e2a4e
epashiq <ashiqashi4715@gmail.com>
2024-04-18 20:54:30 +0530

    create auth model class

---
+++ lib/controller/services/auth_service.dart
@@ -0,0 +1,43 @@
+// import 'dart:convert';
+// import 'dart:developer';
+
+// import 'package:crypto/crypto.dart';
+// import 'package:dio/dio.dart';
+// import 'package:promilo_meetup/core/utils/api_utils.dart';
+// import 'package:promilo_meetup/model/token_model.dart';
+// import 'package:promilo_meetup/model/user_model.dart';
+
+// class AuthService {
+//   static Future<TokenModel?> login(UserModel model) async {
+//     final dio = Dio();
+//     final convertedPassword =
+//         sha256.convert(utf8.encode(model.password.trim())).toString();
+
+//     final formData = FormData.fromMap({
+//       'username': model.email,
+//       'password': convertedPassword,
+//       'grant_type': 'password',
+//     });
+
+//     try {
+//       final response = await dio.post(
+//         ApiUtils.postUrl,
+//         data: formData,
+//         options: Options(
+//           headers: {
+//             'Authorization': 'Basic UHJvbWlsbzpxNCE1NkBaeSN4MiRHQg==',
+//           },
+//         ),
+//       );
+
+//       if (response.statusCode == 200) {
+//         return TokenModel(token: response.data['response']['access_token']);
+//       } else {
+//         log('Status code: ${response.statusCode}');
+//       }
+//     } catch (e) {
+//       log(e.toString());
+//     }
+//     return null;
+//   }
+// }
